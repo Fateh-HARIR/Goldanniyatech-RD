bacb9a24d256448b52efd4799c18528ecc0248ca
Yoann AMAR ASSOULINE <47442098+Goldanniyatech@users.noreply.github.com>
2023-02-22 07:50:41 +0100

    small update

---
+++ Pocket-Cards/Python/Python-Pocket-Card-02-Classes.py
@@ -0,0 +1,25 @@
+###################################
+# Python Pocket Card - 02 Classes #
+###################################
+# Classes & Modules
+
+#Built-in Modules 
+import os
+import sys
+
+
+#################################
+# Basic Classes
+#################################
+
+class Animals: 
+    """ Class without arguments to create any kind of animal """
+
+    # The built-it __init__ function is always executed when the class is initiated
+    def __init__(): 
+        pass # Statement to use as a placeholder. 
+
+# Objects Instantiation
+Dolphin = Animals()
+Dinosaur = Animals() 
+
