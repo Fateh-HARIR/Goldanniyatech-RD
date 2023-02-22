9b0a2768c46c09eb0295cc7882f3a240c5d55796
Yoann AMAR ASSOULINE <47442098+Goldanniyatech@users.noreply.github.com>
2023-02-22 05:10:02 +0100

    Adding the Pocket Cards Project

---
+++ Pocket-Cards/Python/Python-Pocket-Card-01-Code.py
@@ -0,0 +1,33 @@
+#################################
+# Python Pocket Guide - 01 Code #
+#################################
+
+# Reference
+# PEP-8 Style Guides https://peps.python.org/pep-0008/ 
+#                    https://realpython.com/python-pep8/#naming-conventions  
+
+#######################
+# 00 - Commentaries
+#######################
+
+# Single-line Commentaries 
+
+# Multi-line commetaries
+# with the hash mark. 
+
+""" Single line Docstring comment """
+
+""" Multi-line 
+    DocString 
+    Comments
+"""
+
+#######################
+# 01 Basic Data Types 
+#######################
+
+integer_example_one = 33
+integer_example_two = 30594
+
+float_example_one = 30.3
+float_example_two = 4930.45
